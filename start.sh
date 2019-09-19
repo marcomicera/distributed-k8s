e7b5b49bb88ab8262e620cf93d6fe5932eed527a
Marco Micera <marco.micera@gmail.com>
2019-09-19 14:32:52 +0200

    Cloning PKB's repo instead of retrieving last release

--- start.sh
+++ start.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 SLAVE_FOLDER=slave
-PKB_VERSION=1.13.0
-PKB_TAR=v${PKB_VERSION}.tar.gz
-PKB_FOLDER=PerfKitBenchmarker-${PKB_VERSION}
-PKB_URL=https://github.com/GoogleCloudPlatform/PerfKitBenchmarker/archive/${PKB_TAR}
+PKB_REPO=git@github.com:GoogleCloudPlatform/PerfKitBenchmarker.git
 PKB_FLAGS=--kubectl\ $(which kubectl)\ --kubeconfig\ ~/.kube/config
 
 # Installing PerfKit Benchmarker dependencies
-wget $PKB_URL
-tar xzf $PKB_TAR
-rm $PKB_TAR
-mv $PKB_FOLDER $SLAVE_FOLDER
+git clone $PKB_REPO $SLAVE_FOLDER
 cd $SLAVE_FOLDER
 sudo pip install -r requirements.txt
 cd ..
@@ -24,13 +18,13 @@ cd ..
 #
 
 # Fixing $PATH so that the appropriate binaries can be found
-# sudo mkdir -p /etc/systemd/system/kubelet.service.d
-# sudo touch /etc/systemd/system/kubelet.service.d/10-env.conf
-# sudo echo '[Service]
-# > Environment=PATH=/opt/bin:/usr/bin:/usr/sbin:$PATH' > /etc/systemd/system/kubelet.service.d/10-env.conf
+sudo mkdir -p /etc/systemd/system/kubelet.service.d
+sudo touch /etc/systemd/system/kubelet.service.d/10-env.conf
+echo '[Service]
+Environment=PATH=/opt/bin:/usr/bin:/usr/sbin:$PATH' | sudo tee /etc/systemd/system/kubelet.service.d/10-env.conf
 
 # Reboot the node?
 # sudo reboot
 
 # The image is ready to be used by Perfkit:
-# $SLAVE_FOLDER/pkb.py --image=ubuntu_ssh --kubectl\ $(which kubectl)\ --kubeconfig\ ~/.kube/config
+$SLAVE_FOLDER/pkb.py --image=ubuntu_ssh $PKB_FLAGS
