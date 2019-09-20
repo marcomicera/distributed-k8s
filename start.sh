92f836697536dbb670ef0eff7b3eb4316e0a609c
Marco Micera <marco.micera@gmail.com>
2019-09-20 15:28:52 +0200

    Using standard ubuntu docker image

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 . util/config.sh
-KUBERNETES_FLAGS=--cloud=Kubernetes\ --kubectl=$(which kubectl)\ --kubeconfig=$HOME/.kube/config
-BENCHMARKS='iperf'
+KUBERNETES_FLAGS=--cloud=Kubernetes\ --kubectl=$(which kubectl)\ --kubeconfig=$HOME/.kube/config\ --kubernetes_anti_affinity=false
+IMAGE=ubuntu
+BENCHMARKS=fio
 THREADS=4
 
 # The image is ready to be used by Perfkit:
--- util/config.sh
+++ util/config.sh
@@ -2,5 +2,3 @@
 
 # Common env variables
 PKB_FOLDER=pkb
-IMAGE=ubuntu_ssh
-IMAGE_ARCHIVE=docker/$IMAGE.tar.gz
