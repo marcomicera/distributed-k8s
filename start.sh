8bb4015bbd8f7bec9c52925621f4469fd41bd0d0
Marco Micera <marco.micera@gmail.com>
2019-10-08 14:46:04 +0200

    Both config file and flag overrides

--- start.sh
+++ start.sh
@@ -5,7 +5,6 @@ if (( $# < 1 )); then
     echo "Usage: ./start.sh <benchmarks_to_run>"
     exit 1
 fi
-
 VERBOSE=false
 
 # PKB repo
@@ -16,7 +15,7 @@ REPO=git@github.com:marcomicera/PerfKitBenchmarker.git
 THREADS=4
 IMAGE=ubuntu
 PKB_FLAGS=--max_concurrent_threads\ $THREADS\ --image\ $IMAGE
-KUBERNETES_FLAGS=--cloud=Kubernetes\ --kubectl=$(command -v kubectl)\ --kubeconfig=$HOME/.kube/config\ --kubernetes_anti_affinity=false
+KUBERNETES_FLAGS=--kubectl=$(command -v kubectl)\ --kubeconfig=$HOME/.kube/config\
 
 # Which benchmarks can be run on Kubernetes with PKB
 AVAILABLE_BENCHMARKS=(
@@ -33,16 +32,17 @@ AVAILABLE_BENCHMARKS=(
 )
 
 # Benchmark-specific flags
+BENCHMARKS_CONFIG_FILE=benchmarks_conf.yaml
 block_storage_workload_FLAGS=
 cassandra_ycsb_FLAGS=
 cassandra_stress_FLAGS=
 cluster_boot_FLAGS=
 fio_FLAGS=
 iperf_FLAGS=
-mesh_network_FLAGS=--num_connections=1\ --num_iterations=1\ --num_vms=10
+mesh_network_FLAGS=--config_override=mesh_network.vm_groups.default.num_connections=1\ ----config_override=mesh_network.vm_groups.default.num_iterations=1\ --config_override=mesh_network.vm_groups.default.vm_count=10
 mongodb_ycsb_FLAGS=
 netperf_FLAGS=
-redis_FLAGS=--redis_clients=$((THREADS - 1))
+redis_FLAGS=----config_override=redis.vm_groups.default.redis_clients=$((THREADS - 1))
 
 # Info
 if [ "$VERBOSE" = true ] ; then
@@ -69,7 +69,7 @@ for BENCHMARK_TO_RUN in "$@"; do
   if [[ " ${AVAILABLE_BENCHMARKS[@]} " =~ ${BENCHMARK_TO_RUN} ]]; then
     declare "BENCHMARK_FLAGS=${BENCHMARK_TO_RUN}_FLAGS"
     echo Running the "$BENCHMARK_TO_RUN" benchmark with the following flags: "${!BENCHMARK_FLAGS}"...
-    $PKB_FOLDER/pkb.py $PKB_FLAGS --benchmarks=$BENCHMARK_TO_RUN $BENCHMARK_FLAGS $KUBERNETES_FLAGS
+    $PKB_FOLDER/pkb.py $PKB_FLAGS --benchmarks=$BENCHMARK_TO_RUN $KUBERNETES_FLAGS --benchmark_config_file=$BENCHMARKS_CONFIG_FILE $BENCHMARK_FLAGS
     echo ...done with "$BENCHMARK_TO_RUN".
   else
     echo "$BENCHMARK_TO_RUN" is not supported. Skipping it...
